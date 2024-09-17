835c741f6b5707aee6370820eac449d69cce09a6
Chanthai1102 <chanthaithy.bs2021@gmail.com>
2024-09-17 16:45:41 +0700

    setup project

---
+++ src/test/java/com/kiloit/onlyadmin/OnlyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiloit.onlyadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
