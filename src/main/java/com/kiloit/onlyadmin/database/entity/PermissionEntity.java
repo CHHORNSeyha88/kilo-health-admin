9fd40f45aa46acb51a43ae252aa33b581bafe111
ratana <chhanratta92000@gmail.com>
2024-09-18 15:31:47 +0700

    user management

--- src/main/java/com/kiloit/onlyadmin/database/entity/PermissionEntity.java
+++ src/main/java/com/kiloit/onlyadmin/database/entity/PermissionEntity.java
@@ -1,7 +1,11 @@
 package com.kiloit.onlyadmin.database.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.kiloit.onlyadmin.base.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,4 +18,7 @@ public class PermissionEntity extends BaseEntity {
     private String code;
     private String name;
     private String module;
+
+    @ManyToMany(mappedBy = "permissions")
+    List<RoleEntity> roles = new ArrayList<>();
 }
