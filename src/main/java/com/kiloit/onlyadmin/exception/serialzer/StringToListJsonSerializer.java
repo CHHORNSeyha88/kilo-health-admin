7c838ab77074408835c7d03a75be87517d50bd2f
ratana <chhanratta92000@gmail.com>
2024-09-19 02:36:54 +0700

    just test update package in JsonSerializer

--- src/main/java/com/kiloit/onlyadmin/exception/serialzer/StringToListJsonSerializer.java
+++ src/main/java/com/kiloit/onlyadmin/exception/serialzer/StringToListJsonSerializer.java
@@ -1,5 +1,4 @@
-package org.chanthai.ecommerce.exception.serialzer;
-
+package com.kiloit.onlyadmin.exception.serialzer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
