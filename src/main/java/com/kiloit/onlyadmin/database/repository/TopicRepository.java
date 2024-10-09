5c345d7acfc99fb7308f04294c20d3e8d6752ccd
Chanthai1102 <chanthaithy.bs2021@gmail.com>
2024-10-09 16:26:56 +0700

    update env

--- src/main/java/com/kiloit/onlyadmin/database/repository/TopicRepository.java
+++ src/main/java/com/kiloit/onlyadmin/database/repository/TopicRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TopicRepository extends JpaRepository<TopicEntity, Long> {
-    Optional<TopicEntity> findByIdAndDeletedAtNull(Long id);
+//    Optional<TopicEntity> findByIdAndDelete(Long id);
 }
