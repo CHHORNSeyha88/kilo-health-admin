cdce8ca318d0786f97e97ccf37fe3f7f50d6bdad
Nam Phathyuth <phatyuth666@gmail.com>
2024-10-30 08:37:49 +0000

    Update FileMediaService.java

--- src/main/java/com/kiloit/onlyadmin/service/FileMediaService.java
+++ src/main/java/com/kiloit/onlyadmin/service/FileMediaService.java
@@ -43,7 +43,7 @@ public class FileMediaService extends BaseService {
     public FileMediaResponse FileUpload(MultipartFile fileUpload){
         String extension = fileUpload.getContentType().split("/")[1];
         String newName = UUID.randomUUID().toString();
-        if (Files.notExists(Path.get(serverPath))) Files.createDirectories(serverPath);
+        if (Files.notExists(Paths.get(serverPath))) Files.createDirectories(Paths.get(serverPath));
         
         try {
             Files.copy(fileUpload.getInputStream(),Paths.get(serverPath+newName+"."+extension));
